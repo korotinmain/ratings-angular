f6691d3177c22a8935e76312c4d6e9e3f4b960d5
Denys Korotin <deniskorotin4@gmail.com>
2018-06-03 14:36:07 +0300

    add formcard logic

--- src/app/rating-app/components/formcard/formcard.component.ts
+++ src/app/rating-app/components/formcard/formcard.component.ts
@@ -1,15 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {animate, style, transition, trigger} from '@angular/animations';
 
 @Component({
   selector: 'app-formcard',
   templateUrl: './formcard.component.html',
-  styleUrls: ['./formcard.component.scss']
+  styleUrls: ['./formcard.component.scss'],
+  animations: [
+    trigger(
+      'enterAnimation', [
+        transition(':enter', [
+          style({transform: 'translateX(100%)', opacity: 0}),
+          animate('500ms', style({transform: 'translateX(0)', opacity: 1}))
+        ]),
+        transition(':leave', [
+          style({transform: 'translateX(0)', opacity: 1}),
+          animate('500ms', style({transform: 'translateX(100%)', opacity: 0}))
+        ])
+      ]
+    )
+  ]
 })
-export class FormcardComponent implements OnInit {
+export class FormcardComponent {
 
-  constructor() { }
+  @Input() form;
 
-  ngOnInit() {
+  @Input() marks;
+
+  @Input() isVisibility;
+
+  @Output() changeVisibility: EventEmitter<any> = new EventEmitter();
+  @Output() addPerson: EventEmitter<any> = new EventEmitter();
+
+  onAddPerson(event) {
+    this.addPerson.emit(this.form);
   }
-
+  onChangeVisibility(event){
+    this.changeVisibility.emit();
+  }
 }
