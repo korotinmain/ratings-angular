0c47df2fa4b6e848a3dafd9e44b93904d83ebee4
Denys Korotin <deniskorotin4@gmail.com>
2018-06-03 14:40:21 +0300

    add toolbar component

--- src/app/rating-app/components/toolbar/toolbar.component.ts
+++ src/app/rating-app/components/toolbar/toolbar.component.ts
@@ -1,15 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-toolbar',
   templateUrl: './toolbar.component.html',
   styleUrls: ['./toolbar.component.scss']
 })
-export class ToolbarComponent implements OnInit {
+export class ToolbarComponent {
 
-  constructor() { }
+  @Input() dataSource;
+  @Input() isVisibility;
 
-  ngOnInit() {
-  }
 
 }
