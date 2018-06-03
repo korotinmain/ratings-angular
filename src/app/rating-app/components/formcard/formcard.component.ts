b9521dd7f2fcae40cc31fed5369dd711d6a39f4d
Denys Korotin <deniskorotin4@gmail.com>
2018-06-03 13:22:56 +0300

    add-components-to-directory

---
+++ src/app/rating-app/components/formcard/formcard.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-formcard',
+  templateUrl: './formcard.component.html',
+  styleUrls: ['./formcard.component.scss']
+})
+export class FormcardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
